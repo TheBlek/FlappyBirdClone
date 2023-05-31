01023a85a9c8494bd19725ec7aba2de2f42336d9
Blek <egorka99966@gmail.com>
2023-05-31 14:10:47 +0700

    start drawing a pipe. does not work `cause the resource loading stuff

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ use bevy::prelude::*;
 const UP_SPEED: f32 = 500.0;
 const GRAVITY: f32 = -2000.0;
 const ANGLE_AMPLITUDE: f32 = 0.8;
+const PIPE_WINDOW_SIZE: f32 = 200.0;
 
 #[derive(Component)]
 struct Velocity(Vec3);
@@ -17,7 +18,7 @@ struct PlayerBundle {
     marker: Player,
 }
 
-fn startup(mut commands: Commands, asset_server: Res<AssetServer>) {
+fn startup(mut commands: Commands, asset_server: Res<AssetServer>, images: Res<Assets<Image>>) {
     commands.spawn(Camera2dBundle::default());
     commands.spawn(PlayerBundle {
         sprite: SpriteBundle {
@@ -27,6 +28,28 @@ fn startup(mut commands: Commands, asset_server: Res<AssetServer>) {
         velocity: Velocity(Vec3::ZERO),
         marker: Player,
     });
+
+    let pipe_start = asset_server.load("sprites/pipe.png");
+    let pipe_start_height = images.get(&pipe_start).unwrap().size().y;
+
+    let pipe_segment = asset_server.load("sprites/pipe_piece.png");
+    let pipe_segment_height = images.get(&pipe_segment).unwrap().size().y;
+
+    let lower_pipe_bundle = SpriteBundle {
+        texture: pipe_start,
+        transform: Transform {
+            translation: Vec3::Y * -(pipe_start_height + PIPE_WINDOW_SIZE)/2.0,
+            ..default()
+        },
+        ..default()
+    };
+
+    let mut higher_pipe_bundle = lower_pipe_bundle.clone();
+    higher_pipe_bundle.sprite.flip_y = true;
+    higher_pipe_bundle.transform.translation *= -1.0;
+
+    commands.spawn(lower_pipe_bundle);
+    commands.spawn(higher_pipe_bundle);
 }
 
 fn jump(keyboard_input: Res<Input<KeyCode>>, mut query: Query<&mut Velocity, With<Player>>) {
@@ -45,14 +68,14 @@ fn rotate(mut query: Query<(&mut Transform, &Velocity), With<Player>>) {
 }
 
 fn apply_gravity(time: Res<Time>, mut query: Query<&mut Velocity, With<Player>>) {
-    let dt = time.delta().as_secs_f32();
+    let dt = time.delta_seconds();
     for mut velocity in &mut query {
         velocity.0 -= Vec3::NEG_Y * GRAVITY * dt;
     }
 }
 
 fn apply_velocity(time: Res<Time>, mut query: Query<(&Velocity, &mut Transform)>) {
-    let dt = time.delta().as_secs_f32();
+    let dt = time.delta_seconds();
     for (velocity, mut transform) in &mut query {
         transform.translation += velocity.0 * dt;
     }
